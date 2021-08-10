a48453bbe63aa7a3919ecff21fcbadaf92bc9871
Viviane Guimarães <83373229+vivianeguimaraesdev@users.noreply.github.com>
2021-08-10 16:34:05 -0300

    Create funcao.js

---
+++ JavaScriptReativo/Paradigmas/funcao.js
@@ -0,0 +1,9 @@
+//Composição de funções
+
+const exagerado = composicao(
+    gritar, // <- é uma função
+    enfatizar, // <- é uma função
+)
+
+//Exagerado também é uma função 
+exagerado('PARA');
